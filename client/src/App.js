356e96691a8df1cd6277c68e020d25c17078be66
Hadja Hanne <hadja-fatou.hanne@epitech.eu>
2023-01-07 12:16:40 +0100

    socket io client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Page from './components/Chat/Page';
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:5000');
+
 function App() {
+
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<Page socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

732dff6485567fc4ab53b800149f87b571adcf3c
Hadja Hanne <hadja-fatou.hanne@epitech.eu>
2023-01-14 20:52:04 +0100

    props socket for Auth component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Page from './components/Chat/Page';
 import socketIO from 'socket.io-client';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Auth from './components/Auth/Auth';
 const socket = socketIO.connect(process.env.REACT_APP_BASE_URL);
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
           <Route path="/" element={<Login socket={socket} />}></Route>
           <Route path="/chat" element={<Page socket={socket} />}></Route>
           <Route path="/register" element={<Register socket={socket} />}></Route>
+
+          <Route path="/euzgyjdk" element={<Auth socket={socket} />}></Route>
         </Routes>
       </div>
     </BrowserRouter>

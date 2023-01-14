1ea57edff28b7792af2202f9539495253bc2f89c
Hadja Hanne <hadja-fatou.hanne@epitech.eu>
2023-01-14 20:53:52 +0100

    retrieving active users, channels and all users

--- client/src/components/Chat/SideBar.js
+++ client/src/components/Chat/SideBar.js
@@ -1,18 +1,94 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function SideBar() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+function SideBar({ socket }) {
+
+    const [activeUsers, setActiveUsers] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [channels, setChannels] = useState([]);
+
+
+    useEffect(() => {
+        const getACtiveUsers = () => {
+            socket.on("activeUsers", (data) => {
+                setActiveUsers(JSON.parse(data));
+            })
+        };
+        getACtiveUsers();
+    }, [socket, activeUsers]);
+
+
+    useEffect(() => {
+        console.log(activeUsers)
+    }, [activeUsers]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            await axios.get(process.env.REACT_APP_BASE_URL + 'users').then(res => {
+                const result = res.data.users;
+                setUsers(result);
+            });
+        };
+        getUsers();
+
+
+    }, [users]);
+
+
+    useEffect(() => {
+        const getChannels = async () => {
+            await axios.get(process.env.REACT_APP_BASE_URL + 'list').then(res => {
+                const result = res.data.channels;
+                setChannels(result);
+            });
+        };
+
+        getChannels();
+
+
+
+    }, [channels])
+
+
+
+
+    //setTimeout(() => console.log(activeUsers),5);
 
     return (<div className="chat__sidebar">
         <h2>Chat App</h2>
 
         <div>
-            <h4 className="chat__header">ACTIVE USERS</h4>
-            <div className="chat__users">
-                <p>User 1</p>
-                <p>User 2</p>
-                <p>User 3</p>
-                <p>User 4</p>
-            </div>
+            <h4 className="chat__header"> USERS</h4>
+
+            {users.map(user => {
+                return <div key={user._id} className="chat__users">
+                    <p> {user.username}</p>
+                </div>
+            })}
+
+
+            <h4 className="chat__header">CHANNELS</h4>
+            <p> New channel <FontAwesomeIcon icon={faPlus} /> </p>
+
+            {channels.map(channel => {
+                return <div key={channel._id} className="chat__users">
+                    <p> {channel.name}</p>
+                </div>
+            })}
+
+            <p> active users</p>
+            {activeUsers.map(user => {
+                return <div key={user[0]._id} className="chat__users" >
+                    <p> {user[0].username}</p>
+                </div>
+            })}
+
+
         </div>
     </div>);
 }
